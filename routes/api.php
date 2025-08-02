d0839d23d09fff26bdfa2d340928c28058decd3e
Fernando Santana <bh.carvalho@gmail.com>
2025-08-02 20:40:21 -0300

    adapt to e2e

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\Api\RecipeController;
 use App\Http\Controllers\Api\CategoryController;
+
+if (app()->environment('e2e')) {
+    Route::post('/test-reset', fn () => Artisan::call('migrate:fresh --seed'));
+}
 
 Route::get('/recipes', [RecipeController::class, 'index']);
 Route::get('/recipes/{id}', [RecipeController::class, 'show']);
