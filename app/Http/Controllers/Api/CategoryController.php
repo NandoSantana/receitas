5e03c3dbb477c8875469beb2937fc08216b2bdde
Fernando Santana <bh.carvalho@gmail.com>
2025-08-02 20:28:03 -0300

    recipe controller with purifier

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
     //
+    public function index()
+    {
+        return Category::all();
+    }
+
 }
--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -2,10 +2,36 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Recipe;
+use Mews\Purifier\Purifier;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class RecipeController extends Controller
 {
     //
+
+    public function index(Request $request)
+    {
+        $query = Recipe::with('category');
+        $category_id = Purifier::clean($request->input('category_id'));
+   
+
+        if ($request->has('category_id')) {
+            $query->where('category_id', $category_id);
+        }
+
+        return response()->json($query->paginate(6));
+    }
+
+    public function show($id)
+    {
+        return Recipe::with('category')->findOrFail($id);
+    }
+
+    public function store()
+    {
+
+    }
+
 }
