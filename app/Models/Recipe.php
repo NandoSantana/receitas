c603df7243afb0e4eac59daaad574b9a52ec931c
Fernando Santana <bh.carvalho@gmail.com>
2025-08-03 09:56:10 -0300

    env endtoend and models filable

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -11,6 +11,9 @@ class Recipe extends Model
     /** @use HasFactory<\Database\Factories\RecipeFactory> */
     use HasFactory;
 
+
+    protected $fillable = ['title', 'description', 'image_path', 'category_id'];
+
     public function category()
     {
         return $this->belongsTo(Category::class);
